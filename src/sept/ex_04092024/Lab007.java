d2724b53e60da9d4a26605807fe1312a05b1e05e
Sunil Kumar Chhualsingh <rajucsingh@gmail.com>
2024-09-08 12:36:58 +0530

    Add the files

---
+++ src/sept/ex_04092024/Lab007.java
@@ -0,0 +1,22 @@
+package sept.ex_04092024;
+/* Author - Pramod
+ *Program - ATBx
+ * Program to explain comments
+ *
+ */
+
+public class Lab007 {
+    public static void main(String[] args) {
+          //  Single line comment
+
+          // This code will not be executed.
+          // This is used to show the user or dev instructions
+          // You can write anything here
+
+        /* Multi line comment
+        *
+        *  Write anything here
+        **/
+        System.out.println("Hi, Sunil");
+    }
+}
---
+++ src/sept/ex_04092024/Lab008.java
@@ -0,0 +1,16 @@
+// Below code is package : folder with sept -> ex_06 folder
+package sept.ex_04092024;
+/* Author : Pramod
+*
+* Write anything here
+**/
+
+// Java program with class file, name of the class is Lab008
+public class Lab008 {
+// This is the main function, JVM starts from here
+    public static void main(String[] args) {
+// Below code will be executed, it will print hello world
+        System.out.println("Hello World!");
+
+    }
+}
