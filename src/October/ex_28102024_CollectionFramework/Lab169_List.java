3b5059577570c6551e0003b568f0b17173a401b5
Sunil Kumar Chhualsingh <rajucsingh@gmail.com>
2024-11-04 20:33:21 +0530

    Add the files

---
+++ src/October/ex_28102024_CollectionFramework/Lab169_List.java
@@ -0,0 +1,14 @@
+package October.ex_28102024_CollectionFramework;
+
+import java.util.List;
+
+public class Lab169_List {
+    public static void main(String[] args) {
+        // List list  = new List();
+        List list = List.of("banana","apple","grapes","orange");
+        System.out.println(list);
+        System.out.println(list.size());
+        // list.add("guava"); // UnsupportedOperationException
+
+    }
+}
---
+++ src/October/ex_28102024_CollectionFramework/Lab174_ArrayList.java
@@ -0,0 +1,20 @@
+package October.ex_28102024_CollectionFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab174_ArrayList {
+
+    public static void main(String[] args) {
+
+        // List<int> l = new ArrayList();
+        List<Integer> l = new ArrayList();
+        l.add(123);
+        // l.add("Pramod");  -<> - Integer
+
+        List<String> names = new ArrayList();
+        names.add("pramod");
+        // names.add(234);
+
+    }
+}
